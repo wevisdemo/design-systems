61833cd30df50f0754fe51cf584d14e3e52afa4d
Th1nkK1D <witheep@gmail.com>
2022-01-19 18:24:55 +0200

    Add vitepress, move to monorepo and config turborepo

---
+++ docs/src/index.md
@@ -0,0 +1,8 @@
+<script setup>
+  import Navbar from '../../ui/build/components/navbar.vue'
+</script>
+
+# Wevis Design Systems
+
+test import vue navbar
+<Navbar />
--- ui/scripts/compile-mitosis.mjs
+++ ui/scripts/compile-mitosis.mjs
@@ -8,7 +8,7 @@ const event = process.argv[process.argv.length - 2];
 if (event === 'change' || event === 'add') {
   const input = process.argv[process.argv.length - 1];
   const filename = input.split('/').reverse()[0].split('.')[0];
-  const output = `${OUTPUT_DIR}/${filename}.jsx`;
+  const output = `${OUTPUT_DIR}/${filename}.vue`;
 
-  await $`yarn mitosis compile -t=react --out=${output} ${input} --force`;
+  await $`yarn mitosis compile -t=vue --out=${output} ${input} --force`;
 }
