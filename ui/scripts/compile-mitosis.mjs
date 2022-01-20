008c96c2a358b309f933a98ef0a6be43aee68bde
Th1nkK1D <witheep@gmail.com>
2022-01-20 16:53:11 +0200

    Add ui build command

--- ui/scripts/compile-mitosis.mjs
+++ ui/scripts/compile-mitosis.mjs
@@ -2,13 +2,32 @@
 import 'zx/globals';
 
 const OUTPUT_DIR = 'components';
+const TARGET_EXTENSION = {
+  vue: 'vue',
+  react: 'jsx',
+  svelte: 'svelte',
+};
 
-const event = process.argv[process.argv.length - 2];
-
-if (event === 'change' || event === 'add') {
-  const input = process.argv[process.argv.length - 1];
+async function compile(input, target) {
   const filename = input.split('/').reverse()[0].split('.')[0];
-  const output = `${OUTPUT_DIR}/${filename}.vue`;
+  const output = `${OUTPUT_DIR}/${filename}.${TARGET_EXTENSION[target]}`;
 
-  await $`yarn mitosis compile -t=vue --out=${output} ${input} --force`;
+  await $`yarn mitosis compile -t=${target} --out=${output} ${input} --force`;
 }
+
+const targets = process.argv[process.argv.length - 2]?.split(',');
+
+if (targets?.length === 0) {
+  console.error(`Invalid target: ${target}`);
+  process.exit(1);
+}
+
+const input = process.argv[process.argv.length - 1];
+
+let inputs = await globby(input);
+
+inputs.forEach((input) => {
+  targets.forEach((target) => {
+    compile(input, target);
+  });
+});
