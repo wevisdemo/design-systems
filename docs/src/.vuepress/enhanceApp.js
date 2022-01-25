fd15c843910a669b313b609b4c88d0f439f24b1b
Th1nkK1D <witheep@gmail.com>
2022-01-25 21:51:15 +0200

    Reorganize UI css files

--- docs/src/.vuepress/enhanceApp.js
+++ docs/src/.vuepress/enhanceApp.js
@@ -1,7 +1,8 @@
 import WvNavbar from '@wevisdemo/ui/components/navbar.vue';
 import WvFooter from '@wevisdemo/ui/components/footer.vue';
 
-import '@wevisdemo/ui/components/styles.css';
+import '@wevisdemo/ui/styles/typography.css';
+import '@wevisdemo/ui/styles/components.css';
 
 export default ({ Vue }) => {
   Vue.component('WvNavbar', WvNavbar);
