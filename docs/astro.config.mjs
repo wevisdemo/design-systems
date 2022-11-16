ebeff782be29b2e2c64fb80434869a312e3d6cdd
rootEnginear <rootEnginear@users.noreply.github.com>
2022-11-16 20:34:46 +0700

    fix: use proper base

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,5 +6,5 @@ import mdx from '@astrojs/mdx';
 export default defineConfig({
   integrations: [preact(), react(), mdx()],
   site: `https://wevisdemo.github.io`,
-  base: process.env.PUBLIC_ASTRO_BASE || '/',
+  base: import.meta.env.PUBLIC_ASTRO_BASE ?? '/',
 });
