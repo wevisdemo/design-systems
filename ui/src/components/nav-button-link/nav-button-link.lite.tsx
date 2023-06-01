56c5ac1f75206a0f950e0912f1b55a582e61beda
rootEnginear <rootEnginear@users.noreply.github.com>
2023-06-01 20:44:48 +0700

    feat: new `nav-button-link` component

---
+++ ui/src/components/nav-button-link/nav-button-link.lite.tsx
@@ -0,0 +1,32 @@
+import { useDefaultProps } from '@builder.io/mitosis';
+import { JSX } from '@builder.io/mitosis/jsx-runtime';
+
+interface WvNavButtonLinkProps {
+  href: string;
+  target?: string | undefined;
+  rel?: string | undefined;
+  dark?: boolean;
+  children?: JSX.Element | JSX.Element[] | string | number;
+}
+
+export default function WvNavButtonLink(props: WvNavButtonLinkProps) {
+  useDefaultProps<WvNavButtonLinkProps>({
+    href: '',
+    target: '_blank',
+    rel: 'nofollow noopener noreferrer',
+    dark: false,
+  });
+
+  return (
+    <a
+      href={props.href}
+      target={props.target || undefined}
+      rel={props.rel || undefined}
+      class={`wv_nav-button-link wv-ibmplex wv-b7 ${
+        props.dark ? 'wv_nav-button-link--white' : 'wv_nav-button-link--black'
+      }`}
+    >
+      {props.children}
+    </a>
+  );
+}
