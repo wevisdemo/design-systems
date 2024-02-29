b97584e67ea166dbf714d36a180ae32cb57a561e
Poppap <pimpatipan@punchup.world>
2024-02-29 16:27:16 +0700

    feat: change vue2 to vue3

--- ui/scripts/build-mitosis.sh
+++ ui/scripts/build-mitosis.sh
@@ -4,6 +4,6 @@ npx mitosis build --config=mitosis.config.cjs
 # Flatten build dir
 find .mitosis/react/src -wholename "*.tsx" -exec mv '{}' .mitosis/react \;
 find .mitosis/svelte/src -wholename "*.svelte" -exec mv '{}' .mitosis/svelte \;
-find .mitosis/vue2/src -wholename "*.vue" -exec mv '{}' .mitosis/vue2 \;
+find .mitosis/vue/src -wholename "*.vue" -exec mv '{}' .mitosis/vue \;
 
 rm -r .mitosis/*/src
