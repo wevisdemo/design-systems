9c464df7b98bd1f8cb90b75206c0a73e94a06956
Th1nkK1D <witheep@gmail.com>
2024-03-20 11:50:16 +0700

    feat(ui): generate vue .d.ts type

--- ui/scripts/build-mitosis.sh
+++ ui/scripts/build-mitosis.sh
@@ -5,5 +5,7 @@ npx mitosis build --config=mitosis.config.cjs
 find .mitosis/react/src -wholename "*.tsx" -exec mv '{}' .mitosis/react \;
 find .mitosis/svelte/src -wholename "*.svelte" -exec mv '{}' .mitosis/svelte \;
 find .mitosis/vue/src -wholename "*.vue" -exec mv '{}' .mitosis/vue \;
-
 rm -r .mitosis/*/src
+
+# Remove JSX type from vue file
+find .mitosis/vue -type f -wholename "*.vue" -exec sed -i 's/JSX.Element/Element/g' {} \;
