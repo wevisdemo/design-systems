0d5183eda8168c9458500e6266c3a875de5a6fc5
Th1nkK1D <witheep@gmail.com>
2024-03-20 16:04:43 +0700

    fix(ui): use default void function for optional function prop

--- ui/src/components/nav-button/nav-button.lite.tsx
+++ ui/src/components/nav-button/nav-button.lite.tsx
@@ -4,7 +4,7 @@ import { JSX } from '@builder.io/mitosis/jsx-runtime';
 interface WvNavButtonProps {
   dark: boolean;
   active: boolean;
-  onClick?: () => void;
+  onClick: () => void;
   children?: JSX.Element | JSX.Element[] | string | number;
 }
 
@@ -12,7 +12,7 @@ export default function WvNavButton(props: WvNavButtonProps) {
   useDefaultProps<WvNavButtonProps>({
     dark: false,
     active: false,
-    onClick: undefined,
+    onClick: () => {},
   });
 
   return (
@@ -21,7 +21,7 @@ export default function WvNavButton(props: WvNavButtonProps) {
       class={`wv_nav-button wv-ibmplex wv-b7 ${
         props.dark ? 'wv_nav-button--white' : 'wv_nav-button--black'
       } ${props.active ? 'wv_nav-button--active' : ''}`}
-      onClick={() => props.onClick && props.onClick()}
+      onClick={props.onClick}
     >
       {props.children}
     </button>
