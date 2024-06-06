17c6ee3bc2ef17ea58800fcf97af1ad5059d9617
Th1nkK1D <witheep@gmail.com>
2024-06-06 18:50:02 +0700

    fix: convert react svg property to camel case

--- ui/scripts/build-mitosis.sh
+++ ui/scripts/build-mitosis.sh
@@ -10,3 +10,6 @@ rm -r .mitosis/*/src
 # Remove JSX type from Vue and Svelte file
 find .mitosis/vue -type f -wholename "*.vue" -exec sed -i 's/JSX.Element/Element/g' {} \;
 find .mitosis/svelte -type f -wholename "*.svelte" -exec sed -i 's/JSX.Element/Element/g' {} \;
+
+# Fix React camelcase SVG property (fill-rule, clip-rule) https://github.com/BuilderIO/mitosis/issues/526
+find .mitosis/react -type f -wholename "*.tsx" -exec sed -i 's/-rule=/Rule=/g' {} \;
